38f17b7de330aa263080e58e78d05ab5f73e8092
Jerry-124 <216129675+Jerry-124@users.noreply.github.com>
2026-09-08 18:16:28 +0200

    style: split compressed plotting statements

--- main.py
+++ main.py
@@ -34,20 +34,35 @@ def save_plots(directory: Path, road: ReferencePath, reference: np.ndarray, stat
     ax.plot(reference[:, 0], reference[:, 1], label="Frenet reference")
     ax.plot(states[:, 0], states[:, 1], label="MPC tracking")
     ax.set(xlabel="x [m]", ylabel="y [m]", title="Reference vs. actual trajectory")
-    ax.axis("equal"); ax.grid(True); ax.legend(); fig.tight_layout()
-    fig.savefig(directory / "reference_vs_actual.png", dpi=160); plt.close(fig)
+    ax.axis("equal")
+    ax.grid(True)
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig(directory / "reference_vs_actual.png", dpi=160)
+    plt.close(fig)
 
     time = np.arange(len(states)) * dt
     error = np.linalg.norm(states[:, :2] - reference[:, :2], axis=1)
     fig, ax = plt.subplots(figsize=(9, 4))
-    ax.plot(time, error); ax.set(xlabel="time [s]", ylabel="position error [m]", title="Tracking error")
-    ax.grid(True); fig.tight_layout(); fig.savefig(directory / "tracking_error.png", dpi=160); plt.close(fig)
+    ax.plot(time, error)
+    ax.set(xlabel="time [s]", ylabel="position error [m]", title="Tracking error")
+    ax.grid(True)
+    fig.tight_layout()
+    fig.savefig(directory / "tracking_error.png", dpi=160)
+    plt.close(fig)
 
     control_time = time[:-1]
     fig, axes = plt.subplots(2, 1, figsize=(9, 6), sharex=True)
-    axes[0].plot(control_time, controls[:, 0]); axes[0].set_ylabel("acceleration [m/s²]"); axes[0].grid(True)
-    axes[1].plot(control_time, np.rad2deg(controls[:, 1])); axes[1].set_ylabel("steering [deg]"); axes[1].set_xlabel("time [s]"); axes[1].grid(True)
-    fig.tight_layout(); fig.savefig(directory / "control_inputs.png", dpi=160); plt.close(fig)
+    axes[0].plot(control_time, controls[:, 0])
+    axes[0].set_ylabel("acceleration [m/s²]")
+    axes[0].grid(True)
+    axes[1].plot(control_time, np.rad2deg(controls[:, 1]))
+    axes[1].set_ylabel("steering [deg]")
+    axes[1].set_xlabel("time [s]")
+    axes[1].grid(True)
+    fig.tight_layout()
+    fig.savefig(directory / "control_inputs.png", dpi=160)
+    plt.close(fig)
 
 
 if __name__ == "__main__":
